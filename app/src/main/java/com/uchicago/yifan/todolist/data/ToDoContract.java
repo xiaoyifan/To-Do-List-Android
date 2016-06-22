4f7a129062d3e19e627524f94c38e1c895f2e277
xiaoyifan <xiaoyifan@uchicago.edu>
2016-06-22 03:32:40 -0700

    Update with dbhelper and todoentry.

--- app/src/main/java/com/uchicago/yifan/todolist/data/ToDoContract.java
+++ app/src/main/java/com/uchicago/yifan/todolist/data/ToDoContract.java
@@ -1,6 +1,7 @@
 package com.uchicago.yifan.todolist.data;
 
 import android.provider.BaseColumns;
+import android.text.format.Time;
 
 /**
  * Created by Yifan on 6/22/16.
@@ -10,5 +11,26 @@ public class ToDoContract {
     public static final class ToDoEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "todolist";
+
+        // title is stored as a string format
+        public static final String COLUMN_TITLE = "title";
+
+        // Date, stored as long in milliseconds
+        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_TIME = "time";
+
+        // Priority is stored as priority in String format.
+        public static final String COLUMN_PRIORITY = "priority";
+
+    }
+
+    // To make it easy to query for the exact date, we normalize all dates that go into
+    // the database to the start of the the Julian day at UTC.
+    public static long normalizeDate(long startDate) {
+        // normalize the start date to the beginning of the (UTC) day
+        Time time = new Time();
+        time.set(startDate);
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
     }
 }
--- app/src/main/java/com/uchicago/yifan/todolist/data/ToDoDBHelper.java
+++ app/src/main/java/com/uchicago/yifan/todolist/data/ToDoDBHelper.java
@@ -3,6 +3,9 @@ package com.uchicago.yifan.todolist.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.uchicago.yifan.todolist.data.ToDoContract.ToDoEntry;
+
 /**
  * Created by Yifan on 6/22/16.
  */
@@ -17,12 +20,26 @@ public class ToDoDBHelper extends SQLiteOpenHelper{
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + ToDoEntry.TABLE_NAME + " (" +
+                // Why AutoIncrement here, and not above?
+                // Unique keys will be auto-generated in either case.  But for weather
+                // forecasting, it's reasonable to assume the user will want information
+                // for a certain date and all dates *following*, so the forecast data
+                // should be sorted accordingly.
+                ToDoEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
 
+                // the ID of the location entry associated with this weather data
+                ToDoEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                ToDoEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
+                ToDoEntry.COLUMN_TIME + " INTEGER NOT NULL, " +
+                ToDoEntry.COLUMN_PRIORITY + " TEXT NOT NULL;";
+
+        db.execSQL(SQL_CREATE_WEATHER_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + ToDoContract.ToDoEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + ToDoEntry.TABLE_NAME);
         onCreate(db);
     }
 }
